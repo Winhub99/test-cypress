de8e703283c8954ab4839f1a82764ba505b47c3a
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-05-10 15:26:48 +0530

    fundamental of cypress

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,20 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(()=>{
+    cy.visit('/fundamentals')
+  })
+  it('Contains correct header check', () => {
+   // cy.visit('/fundamentals')
+   // cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
+   //cy.get('[data-test="fundamentals-header"]').should('contain.text',"Testing Fundamentals")
+   cy.getDataTest('fundamentals-header').should('contain.text',"Testing Fundamentals")
+
+  })
+  it('Accordian works correctly', () => {
+   // cy.visit('/fundamentals')
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+   cy.get('[data-test="accordian-item-1"] div[role="button"]').click()
+   cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+   cy.get('[data-test="accordian-item-1"] div[role="button"]').click()
+   cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
   })
 })
